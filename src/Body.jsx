77eabb114dfbc69432bf81bb1111d386ece49d5c
Mini Agarwal <miniagarwal2002@gmail.com>
2026-02-15 21:10:31 +0530

    Added mockdata and passed props

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import resList from './utils/mockdata'
 import ResturantCard from './ResturantCard'
 
 const Body = () => {
   return (
     <div>
-        <div className='res-container'><ResturantCard/></div>
+        <div className='res-container'>
+          <ResturantCard resData = {resList?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants[0]}/>
+          <ResturantCard resData = {resList?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants[1]}/>
+          <ResturantCard resData = {resList?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants[2]}/>
+          <ResturantCard resData = {resList?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants[3]}/>
+          <ResturantCard resData = {resList?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants[4]}/>
+          <ResturantCard resData = {resList?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants[5]}/>
+       
+        </div>
     </div>
   )
 }
--- src/ResturantCard.jsx
+++ src/ResturantCard.jsx
@@ -1,13 +1,26 @@
+
+import resList from './utils/mockdata'
 import React from 'react'
 
-const ResturantCard = () => {
+const ResturantCard = (props) => {
+  const {resData} = props;
+  console.log(resData?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+
   return (
     <div className='res-card'>
-        <img />
-        <h4></h4>
-        <h4></h4>
-        <h4></h4>
-        <h4></h4>
+      <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+  resData?.info?.cloudinaryImageId
+}/>
+
+
+       
+        <h4>{resData?.info?.name}</h4>
+        <h4>{resData?.info?.avgRating}</h4>
+
+        <h4>{resData?.info?.sla.deliveryTime + "min"}</h4>
+
+        <h4>{resData?.info?.costForTwo}</h4>
+
+
 
     </div>
   )
