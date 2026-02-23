8b187348ea593598871e83523b75f62844e0ce5b
Mini Agarwal <miniagarwal2002@gmail.com>
2026-02-24 01:00:56 +0530

    Added error router in error page

--- src/Error.jsx
+++ src/Error.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useRouteError } from 'react-router-dom'
 
 const Error = () => {
+    const err = useRouteError()
   return (
-    <div>Error</div>
+    <div>
+        <h1>Oops!!</h1>
+        <h3>Something went wrong</h3>
+        <h3>Error is {err.status} : {err.statusText}</h3>
+    </div>
   )
 }
 
